0470847a0e8e3ed30b7e0bb051cf791648d18ac3
rkrishnan76 <rkrishnan76_us@yahoo.com>
2017-09-24 15:56:51 -0700

    Update BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,73 +5,39 @@ import escapeRegExp from 'escape-string-regexp'
 import Book from './Book'
 
 class BookShelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
 
   render() {
-    const { books } = this.props
-    let currentlyReadingBooks
-    let wantToReadBooks
-    let readBooks
-
-    let match = new RegExp(escapeRegExp("currentlyReadingBooks"), 'i')
-    currentlyReadingBooks = books.filter((book) => match.test(book.shelf) )
-    match = new RegExp(escapeRegExp("wantToReadBooks"), 'i')
-    wantToReadBooks = books.filter((book) => match.test(book.shelf))
-    match = new RegExp(escapeRegExp("readBooks"), 'i')
-    readBooks = books.filter((book) => match.test(book.shelf))
 
     return (
       <div>
-      <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {currentlyReadingBooks.map((book) => (
-                      <li key={book.id}>
-                        <Book book={book}/>
-                      </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {wantToReadBooks.map((book) => (
-                      <li key={book.id}>
-                        <Book book={book}/>
-                      </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {readBooks.map((book) => (
-                      <li key={book.id}>
-                        <Book book={book}/>
-                      </li>
-                    ))}
-                    </ol>
-                  </div>
+      <div className="list-books-title">
+          <h1>Krish's book shelf</h1>
+        </div>
+      <div className="list-books-content">
+          <div>
+            {this.props.shelfName.map((shelf, index) => {
+              return <div key={index} className="bookshelf">
+                <h2 className="bookshelf-title">{Object.values(shelf)}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.myShelfBooks.map(book => {
+                      if(String(Object.keys(shelf)) === book.shelf){
+                        return <Book book={book}
+                                  onShelfChange={this.props.onShelfChange}
+                                  key={book.id}
+                                />
+                        }
+                      }
+                  )}
+                  </ol>
                 </div>
               </div>
-            </div>
-    <div  className="open-search">
-      <Link to='/search' onClick={this.props.onNavigate}>Add a book</Link>
-    </div>
-    </div>
+            })}
+          </div>
+        </div>
+        <div  className="open-search">
+    <Link to='/search' onClick={this.props.onNavigate}>Add a book</Link>
+  </div>
     </div>
   )
 }
