8dd41e5fd37b823ab23094ab9f28fcf5d24b2fdc
rkrishnan76 <rkrishnan76_us@yahoo.com>
2017-09-24 22:25:03 -0700

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -9,10 +9,11 @@ class Book extends Component {
 
   render() {
     const  book  = this.props.book
+  
     return(
       <div className="book">
         <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
                   <select onChange={this.updateShelf} value={book.shelf}>
                          <option value="none" disabled>Move to...</option>
